2a3131d15a4632edc78eb4870a40907f86efdfc7
HMOUDAT OUSSAMA <hmoudat.oussama@gmail.com>
2024-01-10 18:27:08 +0100

    updating Back-End

--- backend/src/main/java/ma/dnaengineering/backend/CsvParserApplication.java
+++ backend/src/main/java/ma/dnaengineering/backend/CsvParserApplication.java
@@ -1,12 +1,11 @@
 package ma.dnaengineering.backend;
 
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class CsvParserApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(CsvParserApplication.class, args);
 	}
 }
